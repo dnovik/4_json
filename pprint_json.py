f5e8b65e59f463000c76b2f831becde2ba449050
dnovik01 <dnovik01@gmail.com>
2018-11-05 11:47:37 +0300

    My version of the task

--- pprint_json.py
+++ pprint_json.py
@@ -2,11 +2,19 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open (file_path, 'r', encoding='utf-8') as f:
+    dict_from_json = json.loads(f.read()
 
 
 def pretty_print_json(data):
-    pass
+    json_from_dict = json.dumps(dict_from_json['features'], 
+                      separators=(',', ':'), 
+                      ensure_ascii=False, 
+                      indent=4, 
+                      skipkeys = True, 
+                      sort_keys=True)
+    
+    print(json_from_dict)
 
 
 if __name__ == '__main__':
